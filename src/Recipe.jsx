9603afec511d03990bd796ce1e89cbe3dd1d15b7
b4jnda <bharath4jinda@gmail.com>
2023-02-23 00:46:30 +0530

    prefinal commit

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -1,47 +1,57 @@
-import {useEffect, useState, React} from 'react'
-import { useLocation} from 'react-router-dom'
+import {useEffect, useState} from "react"
+import {useLocation} from "react-router-dom"
 import "./index.css"
 
-
 function Recipe() {
-    const location = useLocation();
-    const id = location.state.meal
-    const [meal, setMeal] = useState("");
-
+  const location = useLocation()
+  const id = location.state.meal
+  const [meal, setMeal] = useState("")
+  const [instructions, setInstructions] = useState("")
 
   useEffect(() => {
     fetch(
-      `https://api.spoonacular.com/recipes/${id}/information?apiKey=bbcf60d7df554ef9878099bbe1aa5b82&includeNutrition=false` 
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=bbcf60d7df554ef9878099bbe1aa5b82&includeNutrition=false&includeInstructions=true`
     )
-      .then((response) => response.json())
-      .then((data) => {
-        setMeal(data);
-        console.log(data);
+      .then(response => response.json())
+      .then(data => {
+        setMeal(data)
+        setInstructions(data.instructions)
       })
       .catch(() => {
-        console.log("error");
-      });
-  }, [id]);
+        console.log("error")
+      })
+  }, [id])
 
-
-     
-     return (
-      <div className="image-container">
-        <img style={{ width: "auto", }} src={meal.image} alt="recipe" className='image'></img>
-        <p >pricePerServing {meal.pricePerServing}</p>
-        <p>readyInMinutes {meal.readyInMinutes}</p>
-        <p>servings {meal.servings} </p>
-        <ul>
-        {/* mealData.meals.map(meal => {
-                return <Meal key={meal.id} meal={meal} />
-        ) */}
-        </ul>
-      
+  return (
+    <div className="image-container">
+      <img
+        style={{width: "auto"}}
+        src={meal.image}
+        alt="recipe"
+        className="image"
+      />
+      <div className="info-container">
+        <div className="info-item">
+          <span className="info-label">Servings:</span>
+          <span className="info-value">{meal.servings}</span>
+        </div>
       </div>
-    );
-  }
-  
-
-
+      <ul>
+        {/* {mealData.meals.map(meal => {
+          return <Meal key={meal.id} meal={meal} />
+        })} */}
+      </ul>
+      <div className="instructions">
+        <h2>Instructions</h2>
+        <ol>
+          {instructions &&
+            instructions
+              .split(".")
+              .map((step, index) => <li key={index}>{step.trim()}</li>)}
+        </ol>
+      </div>
+    </div>
+  )
+}
 
 export default Recipe
